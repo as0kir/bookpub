e7e9ef73dd937dead59a076874a39242b0caab67
Aleksei Kirillov <askir@inbox.ru>
2018-08-14 08:53:58 +1000

    Creating tests using Mockito to mock DB

--- src/main/java/org/test/bookpub/BookPubApplication.java
+++ src/main/java/org/test/bookpub/BookPubApplication.java
@@ -3,15 +3,20 @@ package org.test.bookpub;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.test.bookpubstarter.dbcount.DbCountRunner;
 import org.test.bookpubstarter.dbcount.EnableDbCounting;
 
 import java.util.Collection;
-
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(excludeFilters = @ComponentScan.Filter(UsedForTesting.class))
 @EnableScheduling
 @EnableDbCounting
 @SpringBootApplication
@@ -26,3 +31,5 @@ public class BookPubApplication {
 		return new StartupRunner();
 	}
 }
+
+@interface UsedForTesting {}
---
+++ src/test/java/org/test/bookpub/PublisherRepositoryTests.java
@@ -0,0 +1,35 @@
+package org.test.bookpub;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.test.bookpub.repository.PublisherRepository;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {BookPubApplication.class, TestMockBeansConfig.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PublisherRepositoryTests {
+    @Autowired
+    private PublisherRepository repository;
+
+    @Before
+    public void setupPublisherRepositoryMock(){
+        Mockito.when(repository.count()).thenReturn(1L);
+    }
+
+    @Test
+    public void publicherExists(){
+        assertEquals(1, repository.count());
+    }
+
+    @After
+    public void resetPublicherRepositoryMock(){
+        Mockito.reset(repository);
+    }
+}
